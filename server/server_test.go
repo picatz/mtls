b92ce80509cd236034f6f755ed6deadfca3adf5e
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2021-02-05 00:48:19 -0500

    Start using stretchr/testify/require in tests

--- server/server_test.go
+++ server/server_test.go
@@ -11,60 +11,48 @@ import (
 	"github.com/picatz/mtls/cert"
 	"github.com/picatz/mtls/client"
 	"github.com/picatz/mtls/tlsconf"
+	"github.com/stretchr/testify/require"
 )
 
-func writeToTempFile(prefix string, data []byte) (string, error) {
+func writeToTempFile(t *testing.T, prefix string, data []byte) (string, error) {
 	file, err := ioutil.TempFile("/tmp", prefix)
-	if err != nil {
-		return "", err
-	}
+	require.NoError(t, err)
+	require.NotNil(t, file)
+
 	written, err := file.Write(data)
-	if written != len(data) {
-		return "", fmt.Errorf("%q expected %d got %d", prefix, written, len(data))
-	}
+	require.NoError(t, err)
+	require.Equal(t, len(data), written)
+
 	err = file.Sync()
-	if err != nil {
-		return "", err
-	}
+	require.NoError(t, err)
+
 	return file.Name(), nil
 }
 
 func TestNewServer(t *testing.T) {
 	caPEM, caPrivKeyPEM, err := cert.NewCA(
-		cert.WithNewECDSAKey(),
-		cert.WithCommonName("ssh.ca.name"),
+		cert.WithCommonName("ca.name"),
 	)
+	require.NoError(t, err)
 
-	caPEMFile, err := writeToTempFile("caPEM", caPEM)
-	if err != nil {
-		t.Fatal(err)
-	}
+	caPEMFile, err := writeToTempFile(t, "caPEM", caPEM)
+	require.NoError(t, err)
 
-	_, err = writeToTempFile("caPrivKeyPEM", caPrivKeyPEM)
-	if err != nil {
-		t.Fatal(err)
-	}
+	_, err = writeToTempFile(t, "caPrivKeyPEM", caPrivKeyPEM)
+	require.NoError(t, err)
 
 	serverPEM, serverPrivKeyPEM, err := cert.NewServerFromCA(
 		bytes.NewReader(caPrivKeyPEM),
 		bytes.NewReader(caPEM),
-		cert.WithNewECDSAKey(),
 		cert.WithCommonName("server.name"),
 	)
+	require.NoError(t, err)
 
-	if err != nil {
-		t.Fatal(err)
-	}
+	serverPEMFile, err := writeToTempFile(t, "serverPEM", serverPEM)
+	require.NoError(t, err)
 
-	serverPEMFile, err := writeToTempFile("serverPEM", serverPEM)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	serverPrivKeyPEMFile, err := writeToTempFile("serverPrivKeyPEM", serverPrivKeyPEM)
-	if err != nil {
-		t.Fatal(err)
-	}
+	serverPrivKeyPEMFile, err := writeToTempFile(t, "serverPrivKeyPEM", serverPrivKeyPEM)
+	require.NoError(t, err)
 
 	s, err := New(
 		WithTLSConfig(
@@ -75,9 +63,8 @@ func TestNewServer(t *testing.T) {
 			),
 		),
 	)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
+
 	defer s.Listener().Close()
 	fmt.Println(s)
 }
@@ -85,59 +72,40 @@ func TestNewServer(t *testing.T) {
 func TestServerClient(t *testing.T) {
 	// 1. Create new CA Key and Cert
 	caPEM, caPrivKeyPEM, err := cert.NewCA(
-		cert.WithNewECDSAKey(),
 		cert.WithCommonName("ca"),
 	)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	caPEMFile, err := writeToTempFile("caPEM", caPEM)
-	if err != nil {
-		t.Fatal(err)
-	}
+	caPEMFile, err := writeToTempFile(t, "caPEM", caPEM)
+	require.NoError(t, err)
 
 	// 2. Create new Server Key and Cert
 	serverPEM, serverPrivKeyPEM, err := cert.NewServerFromCA(
 		bytes.NewReader(caPrivKeyPEM),
 		bytes.NewReader(caPEM),
 		cert.WithCommonName("server.name"),
-		cert.WithNewECDSAKey(),
 	)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	serverPEMFile, err := writeToTempFile("serverPEM", serverPEM)
-	if err != nil {
-		t.Fatal(err)
-	}
+	serverPEMFile, err := writeToTempFile(t, "serverPEM", serverPEM)
+	require.NoError(t, err)
 
-	serverPrivKeyPEMFile, err := writeToTempFile("serverPrivKeyPEM", serverPrivKeyPEM)
-	if err != nil {
-		t.Fatal(err)
-	}
+	serverPrivKeyPEMFile, err := writeToTempFile(t, "serverPrivKeyPEM", serverPrivKeyPEM)
+	require.NoError(t, err)
 
 	// 3. Create new Client Key and Cert
 	clientPEM, clientPrivKeyPEM, err := cert.NewClientFromCA(
 		bytes.NewReader(caPrivKeyPEM),
 		bytes.NewReader(caPEM),
 		cert.WithCommonName("client.name"),
-		cert.WithNewECDSAKey(),
 	)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	clientPEMFile, err := writeToTempFile("clientPEM", clientPEM)
-	if err != nil {
-		t.Fatal(err)
-	}
+	clientPEMFile, err := writeToTempFile(t, "clientPEM", clientPEM)
+	require.NoError(t, err)
 
-	clientPrivKeyPEMFile, err := writeToTempFile("clientPrivKeyPEM", clientPrivKeyPEM)
-	if err != nil {
-		t.Fatal(err)
-	}
+	clientPrivKeyPEMFile, err := writeToTempFile(t, "clientPrivKeyPEM", clientPrivKeyPEM)
+	require.NoError(t, err)
 
 	serverTLSConf := tlsconf.BuildDefaultServerTLSConfig(
 		caPEMFile,
@@ -177,9 +145,8 @@ func TestServerClient(t *testing.T) {
 		),
 		WithHandler(serverHandler),
 	)
-	if err != nil {
-		t.Fatal("new server:", err)
-	}
+	require.NoError(t, err)
+
 	defer s.Shutdown()
 	s.Start()
 
@@ -204,21 +171,15 @@ func TestServerClient(t *testing.T) {
 			clientTLSConfig,
 		),
 	)
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	conn, err := c.Dial()
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	// this is required to complete the handshake and populate the connection state
 	// we are doing this so we can print the peer certificates prior to reading / writing to the connection
 	err = conn.Handshake()
-	if err != nil {
-		t.Fatal("client handshake", err)
-	}
+	require.NoError(t, err)
 
 	tag := fmt.Sprintf("[%s -> %s]", conn.LocalAddr(), conn.RemoteAddr())
 
@@ -230,7 +191,5 @@ func TestServerClient(t *testing.T) {
 	<-serverHandlerDoneForTest
 
 	err = conn.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 }
